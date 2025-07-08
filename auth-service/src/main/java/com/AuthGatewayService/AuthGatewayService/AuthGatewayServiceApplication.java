a3fda6b402fa8b0015dc15201ee04d7c0265c4e3
rolyan <rolyan66@gmail.com>
2025-07-08 18:08:45 -0400

    Subida inicial de auth-service e inventory-service

---
+++ auth-service/src/main/java/com/AuthGatewayService/AuthGatewayService/AuthGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.AuthGatewayService.AuthGatewayService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ auth-service/src/test/java/com/AuthGatewayService/AuthGatewayService/AuthGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AuthGatewayService.AuthGatewayService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
